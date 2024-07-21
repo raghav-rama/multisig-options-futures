9b6a601a5cb0fb026d5fa72d6cea206f13064000
raghav-rama <hackerer528@gmail.com>
2024-07-21 12:05:12 +0530

    fix: add init and update ixn

--- programs/options-futures/src/lib.rs
+++ programs/options-futures/src/lib.rs
@@ -8,9 +8,65 @@ pub mod options_futures {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        ctx.accounts.my_account.authority = *ctx.accounts.signer2.key;
+        ctx.accounts.my_account.data = 0;
+        emit!(Initialized {
+            authority: ctx.accounts.my_account.authority,
+            data: ctx.accounts.my_account.data,
+        });
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {
+        msg!("Greetings from Update");
+        assert_eq!(
+            ctx.accounts.my_account.authority,
+            *ctx.accounts.authority.key
+        );
+        ctx.accounts.my_account.data = data;
+        emit!(Updated { data });
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer2, space = 8 + 8 + 32)]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(mut)]
+    pub signer1: Signer<'info>,
+    #[account(mut)]
+    pub signer2: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub authority: Pubkey,
+    pub data: u64,
+}
+
+#[event]
+pub struct Initialized {
+    pub authority: Pubkey,
+    pub data: u64,
+}
+
+#[event]
+pub struct Updated {
+    pub data: u64,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Unauthorized access")]
+    Unauthorized,
+}
--- tests/options-futures.ts
+++ tests/options-futures.ts
@@ -1,16 +1,64 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { OptionsFutures } from "../target/types/options_futures";
+import * as fs from "fs";
+import { assert } from "chai";
 
 describe("options-futures", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.OptionsFutures as Program<OptionsFutures>;
+  console.log("Program ID", program.programId.toBase58());
+  console.log("Signer", program.provider.publicKey.toBase58());
+
+  const secretKey = JSON.parse(
+    fs.readFileSync("/home/hackerboy/.config/solana/id.json", "utf-8")
+  );
+
+  const signer1 = anchor.web3.Keypair.fromSecretKey(new Uint8Array(secretKey));
+  const signer2 = anchor.web3.Keypair.generate();
+  const myAccount = anchor.web3.Keypair.generate();
+
+  const sendLamportsTxn = new anchor.web3.Transaction();
+  sendLamportsTxn.add(
+    anchor.web3.SystemProgram.transfer({
+      fromPubkey: program.provider.publicKey,
+      toPubkey: signer2.publicKey,
+      lamports: 10000000000,
+    })
+  );
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const sendLamportsTxnId = await anchor.web3.sendAndConfirmTransaction(
+      anchor.getProvider().connection,
+      sendLamportsTxn,
+      [signer1]
+    );
+    console.log("sendLamportsTxnId", sendLamportsTxnId);
+
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        myAccount: myAccount.publicKey,
+        signer1: signer1.publicKey,
+        signer2: signer2.publicKey,
+      })
+      .signers([myAccount, signer2])
+      .rpc();
     console.log("Your transaction signature", tx);
+  });
+  it("Updates", async () => {
+    const tx = await program.methods
+      .update(new anchor.BN(45))
+      .accounts({
+        myAccount: myAccount.publicKey,
+        authority: signer2.publicKey,
+      })
+      .signers([signer2])
+      .rpc();
+    console.log("Updates", tx);
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    assert.ok(account.data.eq(new anchor.BN(45)));
   });
 });
